46a18edc0b59fb7e41695319b28fe845bdf15d2e
rajan2012 <rajansah0020@gmail.com>
2024-05-20 20:52:37 +0200

    fixing

---
+++ all.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import pandas as pd
+
+from diseaseprediction import setup_and_run_symptom_selector
+from drug2disease import filter_diseases_by_drug
+from drug_disease import setup_and_run_drug_lookup
+from drug_review import setup_and_run_drug_review
+from predictinsrance import insurance
+
+# Function to display the disease prediction page
+drugfile="drug_disease_unique.csv"
+drugreview="process_drug_reviews.csv"
+diseaefile="removedlongsym.csv"
+#healthfile="insurance_dataset.csv"
+healthfile="process_healthinsurance.csv"
+# #"allcombo_hl.csv"
+#from drug disease
+diseaselist="uniquedisease_drug_disease.csv"
+#from drug reviews
+reviewdiseaselist="uniqdis_drug_rev.csv"
+#with normlaizeed rating
+normalizedrating="normalized_rating.csv"
+#groupby rating count
+ratingcount="df_rating_cnt.csv"
+druglist="uniquedrug.csv"
+
+
+# File paths for the trained pipeline and vectorizer (replace with actual paths)
+pipeline_path = 'RandomForest_new.pkl'
+vectorizer_path = 'CountVectorizer_random.pkl'
+
+def disease_prediction_page():
+    st.title("Disease Prediction")
+    st.write("This is the main page for disease prediction.")
+    setup_and_run_symptom_selector(diseaefile,pipeline_path,vectorizer_path)
+    # Add your disease prediction code here
+
+# Function to display the drug discovery page
+def drug_discovery_page():
+    setup_and_run_drug_lookup(drugfile,diseaselist)
+    # Add your drug discovery code here
+
+# Function to display the drug reviews page
+def drug_reviews_page():
+    st.title("Drug Reviews")
+    st.write("This is the drug reviews page.")
+    setup_and_run_drug_review(drugreview,reviewdiseaselist,normalizedrating,ratingcount)
+    # Add your drug reviews code here
+
+def drug_disease_page():
+    filter_diseases_by_drug(drugfile,druglist)
+    # Add your drug reviews code here
+
+# Function to display the health insurance page
+def health_insurance_page():
+    st.title("Health Insurance")
+    st.write("This is the health insurance page.")
+    insurance(healthfile)
+    # Add your health insurance code here
+
+# Sidebar for navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("health", ["Disease Prediction", "Drug Discovery","Drug Disease", "Drug Reviews", "Health Insurance"])
+
+# Main content
+if page == "Disease Prediction":
+    disease_prediction_page()
+elif page == "Drug Discovery":
+    drug_discovery_page()
+elif page == "Drug Disease":
+    drug_disease_page()
+elif page == "Drug Reviews":
+    drug_reviews_page()
+elif page == "Health Insurance":
+    health_insurance_page()
+
---
+++ diseaseprediction.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import pandas as pd
+import re
+import pandas as pd
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import Pipeline
+
+
+import pickle
+
+import joblib
+
+from loaddata import load_data
+from preprocess import preprocess_text
+
+
+#this method will only have preprocessing things
+# Add the image as a header
+def preprocess_data(df):
+    #all these need to be removed once i load modified datasets
+    st.image("disease_predictor.png", use_column_width=True)
+    # Strip leading and trailing whitespaces from the 'Disease' and 'Symptoms' columns
+    df.loc[:, 'Disease'] = df['Disease'].str.strip().str.lower()
+    df.loc[:, 'Symptoms'] = df['Symptoms'].str.strip().str.lower()
+    df.loc[:, 'Symptoms'] = df['Symptoms'].str.replace('_', ' ')
+
+    # Drop duplicate records based on 'Disease' and 'Symptoms' columns
+    df = df.drop_duplicates(subset=['Disease', 'Symptoms'])
+
+    # Reset the index
+    df.reset_index(drop=True, inplace=True)
+
+    # Count the occurrences of each disease
+    disease_counts = df['Disease'].value_counts()
+
+    # Filter the DataFrame to include only diseases that occur more than once
+    df = df[df['Disease'].isin(disease_counts[disease_counts > 3].index)]
+
+    return df
+
+
+def setup_and_run_symptom_selector(filename, pipeline_path, vectorizer_path):
+    df = load_data(filename)
+    # Preprocess the data
+    df = preprocess_data(df)
+
+    # Extract distinct list of symptoms from the dataset
+    symptoms_list = df['Symptoms'].str.split(',').explode().unique()
+
+    # Title for the app
+    st.title('Symptom Selector')
+
+    # Create a multi-select box for selecting symptoms
+    #with st.form(key='user_input_form'):
+    selected_symptoms = st.multiselect('Select Symptoms:', symptoms_list)
+
+    # Apply preprocessing to the selected symptoms
+    user_symptoms = ','.join(selected_symptoms)
+
+    # Load the trained pipeline and vectorizer from the pickle files
+    pipeline = joblib.load(pipeline_path)
+    vectorizer = joblib.load(vectorizer_path)
+
+
+    def predict_disease(symptoms):
+        # Preprocess symptoms
+        processed_symptoms = preprocess_text(symptoms)
+        st.write("Processed Symptoms:", processed_symptoms)
+
+        # Predict disease using the trained pipeline
+        user_symptoms_vectorized = vectorizer.transform([processed_symptoms])
+        st.write("Vectorized Symptoms:", user_symptoms_vectorized)
+
+        predicted_disease = pipeline.predict(user_symptoms_vectorized)
+        return predicted_disease[0]
+
+    # Predict disease based on selected symptoms when button is clicked
+    if st.button('Predict Disease'):
+        predicted_disease = predict_disease(user_symptoms)
+        st.write("Predicted Disease:", predicted_disease)
