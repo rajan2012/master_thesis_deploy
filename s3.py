3f4c8cc60e016ff1eb5570217d1b3caee50524b0
rajan2012 <rajansah0020@gmail.com>
2024-05-23 14:53:55 +0200

    adding new changes

--- s3.py
+++ s3.py
@@ -1,7 +1,25 @@
-from loaddata import load_data
+import os
+import boto3
+import pandas as pd
 import streamlit as st
 
-filename="s3://masterthesisrajan/uniquedisease_drug_disease.csv"
-df = load_data(filename)
+# Load AWS credentials from Streamlit secrets
+aws_default_region = st.secrets["aws"]["AWS_DEFAULT_REGION"]
+aws_access_key_id = st.secrets["aws"]["AWS_ACCESS_KEY_ID"]
+aws_secret_access_key = st.secrets["aws"]["AWS_SECRET_ACCESS_KEY"]
 
-st.write(df)
+# Set environment variables
+os.environ["AWS_DEFAULT_REGION"] = aws_default_region
+os.environ["AWS_ACCESS_KEY_ID"] = aws_access_key_id
+os.environ["AWS_SECRET_ACCESS_KEY"] = aws_secret_access_key
+
+# Create an S3 client
+s3_client = boto3.client('s3')
+response = s3_client.get_object(Bucket="test22-rajan", Key="df_rating_cnt.csv")
+
+# Read the CSV file from the response
+file = response["Body"]
+data2 = pd.read_csv(file)
+
+# Display the DataFrame in Streamlit
+st.write(data2)
