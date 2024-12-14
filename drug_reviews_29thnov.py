1ac9aa33b8f144d41191632e74d32b71e5b49c44
rajan <rajansah0020@gmail.com>
2024-12-14 19:00:07 +0100

    Update drug_reviews_29thnov.py

--- drug_reviews_29thnov.py
+++ drug_reviews_29thnov.py
@@ -118,8 +118,8 @@ def setup_and_run_drug_review_new(bucket_name,filename,filename2,filename3,filen
     #df3 = df.drop_duplicates(subset=['drug', 'Disease'], keep='first')
     #df3.loc[:, 'Disease'] = df3['Disease'].str.strip().str.lower()
     #df3.loc[:, 'drug'] = df3['drug'].str.strip()
-    st.write("normal_rating_df")
-    st.write(normal_rating_df)
+    #st.write("normal_rating_df")
+    #st.write(normal_rating_df)
 
     # Extract distinct list of diseases from the dataset
     #disease_list = df3['Disease'].unique()
@@ -148,7 +148,7 @@ def setup_and_run_drug_review_new(bucket_name,filename,filename2,filename3,filen
         #result_df = calculate_weighted_avg_rating(df_rating_count, selected_disease, n)
         result_df = topndrugs(normal_rating_df, selected_disease, n)
 
-        st.write(result_df)
+        #st.write(result_df)
 
         #st.write(result_df[['drug', 'Normalized_Rating']], index=False)
 
